e44b145c349d16498573d4b0cb0342435df38f36
Jakub Orzołek <kubaorz1@gmail.com>
2018-11-26 17:11:42 +0100

    litchfield top 1

---
+++ src/main/java/com/resp/kiedyresp/Controller.java
@@ -0,0 +1,30 @@
+package com.resp.kiedyresp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public static String asd() {
+        return
+                "<center>" + wyliczResp(1, "bossów: ") +
+                wyliczResp(4, "herobriny: ") +
+                wyliczResp(6, "minosów: ") +
+                wyliczResp(12, "avatara: ") + "</center>";
+    }
+
+    private static String wyliczResp(int i, String boss) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime resp = LocalDateTime.of(2018, 11, 24, 16, 3);
+        while (true) {
+            resp = resp.plusMinutes(72 * i);
+            if (resp.isAfter(now)) {
+                return (boss + resp.format(DateTimeFormatter.ofPattern("d-MM H:mm")) + "<br></br>");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/resp/kiedyresp/KiedyrespApplication.java
@@ -0,0 +1,12 @@
+package com.resp.kiedyresp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiedyrespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiedyrespApplication.class, args);
+	}
+}
