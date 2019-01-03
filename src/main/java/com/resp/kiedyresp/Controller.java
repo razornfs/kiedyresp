c0b9e31a0bbd82aee798b494d798b6e2e65ff6aa
Jakub Orzołek <kubaorz1@gmail.com>
2019-01-03 13:07:57 +0100

    litchfield top 1

--- src/main/java/com/resp/kiedyresp/Controller.java
+++ src/main/java/com/resp/kiedyresp/Controller.java
@@ -47,9 +47,8 @@ public class Controller {
         return "<br></br><b><font size = 5>" + "Zbijanie przywołań Avatara" + "</font></b><br></br>" +
                "4 stycznia: Bonkers, Antypatyczny, Chapman, SilenceLife, NiePijTato, Paverell" + "<br></br>" +
                "11 stycznia: Bonkers, Antypatyczny, Slooodziak, WojOnePLK, CeHa, SpeaDreaM" + "<br></br>" +
-               "18 stycznia: Bonkers, Antypatyczny, Hikikomori, Rubiks, Pimber, BilonJP" + "<br></br>" +
+               "18 stycznia: Bonkers, Antypatyczny, Eiv, Rubiks, Pimber, BilonJP" + "<br></br>" +
                "25 stycznia: Bonkers, Antypatyczny, Bagroownik, HotTreasure, D4MN, Ropeflake" + "<br></br>" +
-               "1 lutego: Bonkers, Antypatyczny, Chapman, Eiv, SilenceLife, NiePijTato" + "<br></br>" +
                 "Każda grupa zbija 30 przywołań. Wpisowe: 300b";
     }
 }
