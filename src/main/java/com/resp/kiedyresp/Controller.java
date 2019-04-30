212e91c411a08bbd70abfb60fcca4a3ab051b1ad
Jakub Orzołek <kubaorz1@gmail.com>
2019-04-30 17:21:42 +0200

    litchfield top 1

--- src/main/java/com/resp/kiedyresp/Controller.java
+++ src/main/java/com/resp/kiedyresp/Controller.java
@@ -24,7 +24,7 @@ public class Controller {
 
     private static String wyliczResp(int i, String boss) {
         LocalDateTime now = LocalDateTime.now().plusMinutes(120);
-        LocalDateTime resp = LocalDateTime.of(2019, 4, 30, 10, 0);
+        LocalDateTime resp = LocalDateTime.of(2019, 4, 30, 10, 5);
         while (true) {
             resp = resp.plusSeconds((long) (odstep * i * 60));
             if (resp.isAfter(now)) {
